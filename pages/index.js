5c6a0c8b54271f8435440dba41264addf2638e67
Tanaka930 <yumat9637@gmail.com>
2022-02-15 23:18:34 +0900

    4

--- pages/index.js
+++ pages/index.js
@@ -10,7 +10,7 @@ export default function Home() {
 
       <main>
         <h1 className="title">
-          sssxxxiii
+          sssxxxiiirrr
           Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
 
